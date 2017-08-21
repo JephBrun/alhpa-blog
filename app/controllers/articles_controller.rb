a669d4183bd86c7d1397da460b8084f382fa2044
Jeff Brown <jeff.brown@shopify.com>
2017-08-21 19:42:36 -0400

    added edit and show in list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,18 @@
 class ArticlesController < ApplicationController
 
+def index
+  @articles = Article.all 
+end
+
+
+
 def new
   @article = Article.new
 end
+
+def edit
+  @article = Article.find(params[:id])
+  end
 
 def create
   @article = Article.new(article_params)
@@ -19,6 +29,15 @@ end
     @article = Article.find(params[:id])
   end
 
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "UPDATED"
+    redirect_to article_path(@article)
+  else
+    render edit
+  end
+end
 
 
 private
@@ -26,5 +45,7 @@ private
     params.require(:article).permit(:title, :description)
 
     end
+
+
 
 end
