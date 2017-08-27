73aa5f28e0085506f97fa3b24a6f0d3bf356a515
Jeff Brown <jeff.brown@shopify.com>
2017-08-27 12:55:21 -0400

    detroy added and path to articls

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,14 @@ end
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully destroyed!"
+    redirect_to articles_path
+  end
+
+
 def update
   @article = Article.find(params[:id])
   if @article.update(article_params)
